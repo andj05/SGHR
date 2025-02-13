0e6d84293200e8941ef158cdfe782680356bd9fd
Wilmar Gomez <20240103@itla.edu.do>
2025-02-12 22:13:18 -0400

    Commit de lo mio 2024-0103

---
+++ SGHR.Domain/Entities/Configuration/Tarifas.cs
@@ -0,0 +1,14 @@
+﻿using SGHR.Domain.Base;
+
+namespace SGHR.Domain.Entities.Configuration
+{
+    public sealed class Tarifas : AuditEntity
+    {
+        public int IdTarifa { get; set; }
+        public DateOnly FechaInicio { get; set; }
+        public DateOnly FechaFin { get; set; }
+        public decimal PrecioPorNoche { get; set; }
+        public decimal Descuento { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ SGHR.Persistence/Contex/SGHRContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGHR.Domain.Entities.Users;
+
+namespace SGHR.Persistence.Contex
+{
+    public class SGHRContext : DbContext
+    {
+        public SGHRContext(DbContextOptions<SGHRContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}

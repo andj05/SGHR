624149129f4ba783bcce5ec21e58192a4820f11b
Anddy Josue Jara Ramirez <20240148@itla.edu.do>
2025-03-13 00:33:24 -0400

    Capa Web Terminada

---
+++ SGHR.Web/Controllers/TarifasController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SGHR.Application.Dtos.Tarifas;
+using SGHR.Application.Interfaces;
+
+namespace SGHR.Web.Controllers
+{
+    public class TarifasController : Controller
+    {
+        private readonly ITarifasService tarifasService;
+
+        public TarifasController(ITarifasService tarifasService)
+        {
+            this.tarifasService = tarifasService;
+        }
+
+        // GET: TarifasController
+        public async Task<IActionResult> Index()
+        {
+            var result = await tarifasService.GetAll();
+            if (result.Success != true)
+            {
+                List<TarifasDto> tarifasList = (List<TarifasDto>)result.Data;
+
+                return View(tarifasList);
+            }
+            //tarifas
+            return View();
+        }
+
+
+        // GET: TarifasController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TarifasController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TarifasController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TarifasController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TarifasController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TarifasController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
